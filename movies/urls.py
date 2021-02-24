faea792d58eb012d209ea89f546fad7a760b2257
“Rohit <rohitgoud164@gmail.com>
2021-02-24 12:38:15 +0530

    some changes

--- movies/urls.py
+++ movies/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    re_path(r'^api/v1/movies/(?P<pk>[0-9]+)$', # Url to get update or delete a movie
+    re_path('api/v1/movies/<int:pk>/', # Url to get update or delete a movie
         views.get_delete_update_movie.as_view(),
         name='get_delete_update_movie'
     ),
